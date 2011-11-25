999ee8344128816d632e674e5c8b1afaeacab109
Juho Niemisto <juho.niemisto@eficode.fi>
2011-11-25 15:32:54 +0200

    Add ISBN validator

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,4 +2,16 @@ class SiteController < ApplicationController
   def index
   end
 
+  def isbn
+    if request.post?
+      @isbn = params[:isbn]
+      if IsbnValidator.new.validate(@isbn)
+        flash[:notice] = "#{@isbn} is a valid ISBN!"
+        redirect_to isbn_validator_path
+      else
+        flash[:error] = "#{@isbn} does not look like a valid ISBN!"
+        render :isbn
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Library::Application.routes.draw do
+  match "site/isbn", to: "site#isbn", via: [:get, :post], as: :isbn_validator
   root to: "site#index"
 end
